b3eddef315aed382860089d338827f826c84fddd
ichenkun <ichenkun@gmail.com>
2013-03-05 15:19:24 +0800

    add some change

--- README.md
+++ README.md
@@ -3,32 +3,30 @@ Elow
 
 An implementation of event based control flow
 
-do方法接受两个参数, 第一个指定流程类型（series, paralle, waterfall, queue）, 第二个指定所有任务完成后的回调（每个任务结果会依序保存在数组中）
+do方法接受两个参数, 第一个指定流程类型（series, paralle, waterfall, queue）, 第二个指定所有任务完成后的回调（同时传递结果集，该集合为JSON格式，key为任务名称）
 
 示例:
 
-    var tasks = [
-        function(){
-            console.log('this is one!!');
-            Elow.done(1, 'one result!!'); //任务结束, 触发done事件, 第一个参数指定该任务在数组队列中的序列, 第二个参数为执行结果 
+    var tasks = {
+        task1: function(){
+            Elow.done('task1', 'this is 1!!'); //任务结束, 触发done事件, 第一个参数指定该任务名称, 第二个参数为执行结果 
         },
-        function() {
-            console.log('this is two!!');
-            Elow.done(2, 'two result!!');
+        task2: function() {
+            Elow.done('task2', 'this is 2!!');
         },
-        function(){
-            console.log('this is three!!');
-            Elow.done(3, 'three result!!');
+        task3: function(){
+            Elow.done('task3', 'this is 3!!');
         },
-        function() {
-            console.log('this is four!!');
-            Elow.done(4, 'four result!!');
+        task4: function(){
+            Elow.done('task4', 'this is 4!!');
         },
-        function(){
-            console.log('this is five!!');
-            Elow.done(5, 'five result!!');
+        task5: function() {
+            Elow.done('task5', 'this is 5!!');
+        },
+        task6: function(){
+            Elow.done('task6', 'this is 6!!');
         }
-    ];
+    };
 
     //多个任务依次执行 
     Elow(tasks).do('series', function(){
@@ -40,20 +38,20 @@ do方法接受两个参数, 第一个指定流程类型（series, paralle, water
         console.log('this is end callback');
     });
 
-    //设置worker队列数量，有新的worker可用时，后续排队的任务执行
+    //设置worker队列数量(省略第二个参数时，默认为5)，有新的worker可用时，后续排队的任务执行
     Elow(tasks).do('queue', 3, function(){
         console.log('this is end callback');
     });
 
     var tasks = [
-        function(){
-            Elow.done(1, 'one result!!'); //任务结束, 触发done事件, 第一个参数指定该任务在数组队列中的序列, 第二个参数为执行结果 
+        task1: function(){
+            Elow.done('task1', 'one result!!'); 
         },
-        function(data) {
-            Elow.done(2, data + 'two result!!');
+        task2: function(data) {
+            Elow.done('task2', data + 'two result!!');
         },
-        function(data){
-            Elow.done(3, data + 'three result!!');
+        task3: function(data){
+            Elow.done('task3', data + 'three result!!');
         }
     ];
 
--- elow.js
+++ elow.js
@@ -4,63 +4,69 @@ function Elow(tasks){
     }
 
     this.tasks = tasks;
-    this.tasksCount = tasks.length;
-    
-    this.number = 1; 
+    this.keys = [];
     this.handlers = [];
-    this.result = [];
+    this.result = {};
 
     Elow.done = this._done.bind(this);
+
+    this._init();
 }
 
 Elow.prototype = {
-    do: function(type){
+    do: function(type) {
         this.endCallback = arguments[(arguments.length - 1 || 1)] || function(){};
+        this.workerLimit = (typeof arguments[1] === 'number') ? arguments[1] : 5;
+       
+        if (typeof this['_' + type] === 'function') {
+            this['_' + type].call(this);
+        } 
+        else {
+            console.log('Control Flow Type Err!!');
+        }
+    },
+
+    _init: function() {
+        if (Object.keys) {
+            this.keys = Object.keys(this.tasks);
+        }
+        else {
+            for (var k in this.tasks) {
+                if (this.tasks.hasOwnProperty(k)) {
+                    this.keys.push(k);
+                }
+            }
+        }
+
+        return this;
+    },
+
+    _on: function(taskName, handler) {
+        if (!this.handlers[taskName]) {
+            this.handlers[taskName] = [];
+        }
+        this.handlers[taskName].push(handler);
         
-        switch(type) {
-            case 'series':
-                this._series(this.tasks.shift());
-                break;
-            case 'parallel':
-                this._parallel();
-                break;
-            case 'waterfall':
-                this._waterfall(this.tasks.shift());
-                break;
-            case 'queue':
-                this.workerLimit = arguments[1] || 5;
-                this._queue();
-                break;
-        }
-    },
-
-    _on: function(event, handler){
-        if (!this.handlers[event]) {
-            this.handlers[event] = [];
-        }
-        this.handlers[event].push(handler);
-        
         return this;
     },
 
-    _off: function(event){
-        if (this.handlers[event]) {
-            this.handlers[event] = [];
+    _off: function(taskName) {
+        if (this.handlers[taskName]) {
+            this.handlers[taskName] = [];
         }
 
         return this;
     },
 
-    _done: function(event){
-        var t = 'Task::'+ event;
-        for (var i = 0; i < this.handlers[t].length; i++) {
-            this.handlers[t][i].call(this, arguments[1]);
+    _done: function(taskName) {
+        for (var i = 0; i < this.handlers[taskName].length; i++) {
+            this.handlers[taskName][i].call(this, arguments[1]);
         }
 
         return this;
     },
 
-    _async: function(task, data){
+    _async: function(task, data) {
         var delay = Math.floor(Math.random() * 5) * 100;
         
         setTimeout(function(){
@@ -70,22 +76,15 @@ Elow.prototype = {
         return this;
     },
 
-    _finish: function(){
+    _finish: function() {
         this.endCallback(this.result);
         
         return this;
     },
 
-    _series: function(task){
-        if (task) {
-            this._on('Task::' + this.number, function(res){
-                this.result.push(res);
-                this._off('Task::' + this.number);
-                this._series(this.tasks.shift());
-            }.bind(this));
-
-            this._async(task);
-            this.number += 1;
+    _check: function(factor, callback){
+        if (factor) {
+            callback.call(this);
         }
         else {
             this._finish();
@@ -94,88 +93,92 @@ Elow.prototype = {
         return this;
     },
 
-    _parallel: function(){
-        if (this.tasks.length !== 0) {
-            var self = this,
-                count = 0;
-            
-            this.tasks.forEach(function(task){
-                (function(i){
-                    self._on('Task::' + self.number, function(res){
-                        self.result[i-1] = res;
-                        self._off('Task::' + self.number);
-                        
-                        count += 1;
-                        if (count >= self.tasks.length) {
-                            self._finish();
-                        }
-                    });
+    _series: function() {
+        function process(taskName){
+            this._check(taskName, function(){
+                this._on(taskName, function(res){
+                    this.result[taskName] = res;
+                    this._off(taskName);
+                    process.call(this, this.keys.shift());
+                }.bind(this));
 
-                    self._async(task);
-                    self.number += 1;
-                }(self.number))
+                this._async(this.tasks[taskName]);
             });
         }
-        else {
-            this._finish();
-        }
+
+        process.call(this, this.keys.shift());
 
         return this;
     },
 
-    _waterfall: function(task, data) {
-        if (task) {
-            this._on('Task::' + this.number, function(res){
-                this.result.push(res);
-                this._off('Task::' + this.number);
-                this._waterfall(this.tasks.shift(), res);
-            }.bind(this));
+    _waterfall: function() {
+        function process(taskName, data) {
+            this._check(taskName, function(){
+                this._on(taskName, function(res){
+                    this.result[taskName] = res;
+                    this._off(taskName);
+                    process.call(this, this.keys.shift(), res);
+                }.bind(this));
 
-            this._async(task, data);
-            this.number += 1;
+                this._async(this.tasks[taskName], data);
+            });
         }
-        else {
-            this._finish();
-        }
+        
+        process.call(this, this.keys.shift());
 
         return this;
     },
 
-    _queue: function(){
-        if (this.tasks.length !== 0) {
-            var self = this,
-                count = 0,
-                temp = this.tasks.slice(0),
+    _parallel: function() {
+        this._check(this.keys.length, function(){
+            var count = 0;
+            
+            this.keys.forEach(function(taskName){
+                this._on(taskName, function(res){
+                    this.result[taskName] = res;
+                    this._off(taskName);
+                    
+                    count += 1;
+                    if (count >= this.keys.length) {
+                        this._finish();
+                    }
+                }.bind(this));
+
+                this._async(this.tasks[taskName]);
+            }, this);
+        });
+
+        return this;
+    },
+
+    _queue: function() {
+        this._check(this.keys.length, function(){
+            var count = 0,
+                temp = this.keys.slice(0),
                 workers = temp.splice(0, this.workerLimit);
            
-            function _process(tasks) {
-                tasks.forEach(function(task){
-                    (function(i){
-                        self._on('Task::' + self.number, function(res){
-                            self.result[i-1] = res;
-                            self._off('Task::' + self.number);
-                            
-                            count += 1;
-                            if (count >= self.tasks.length) {
-                                self._finish();
-                            }
-                        });    
-                    }(self.number))
+            function process(workers) {
+                workers.forEach(function(taskName){
+                    this._on(taskName, function(res){
+                        this.result[taskName] = res;
+                        this._off(taskName);
+                        
+                        count += 1;
+                        if (count >= this.keys.length) {
+                            this._finish();
+                        }
 
-                    self._async(task);
-                    self.number += 1;
-                });
-
-                if (temp.length != 0) {
-                    _process([temp.shift()]);
-                }
+                        if (temp.length != 0) {
+                            process.call(this, [temp.shift()]);
+                        }
+                    }.bind(this));    
+        
+                    this._async(this.tasks[taskName]);
+                }, this);
             }
             
-            _process(workers);
-        }
-        else {
-            this._finish();
-        }
+            process.call(this, workers);
+        });
         
         return this;
     }
